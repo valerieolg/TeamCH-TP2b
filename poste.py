c20fcb4bb2c69c106e53057e930f223b69bfaed6
valerieolg <valerieolg@gmail.com>
2026-01-08 15:34:44 -0500

    Commit 2 database.py : ajout de données dans le tableau postes

--- poste.py
+++
@@ -1,9 +0,0 @@
-class Poste:
-    def __init__(self, id, nom_poste, utilisateur, type_poste, sys_exploitation, adresse_ip, statut):
-        self.id = id
-        self.nom_poste = nom_poste
-        self.utilisateur = utilisateur
-        self.type_poste = type_poste
-        self.sys_exploitation = sys_exploitation
-        self.adresse_ip = adresse_ip
-        self.statut = statut
