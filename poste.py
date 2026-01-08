0aed898b618cddb7eb02b2823260a2734da423fb
valerieolg <valerieolg@gmail.com>
2026-01-08 15:45:30 -0500

    poste.py

---
+++ poste.py
@@ -0,0 +1,11 @@
+class Poste:
+    def __init__(self, nom_poste, utilisateur, type_poste, sys_exploitation, adresse_ip, statut, id=None):
+        self.id = id
+        self.nom_poste = nom_poste
+        self.utilisateur = utilisateur
+        self.type_poste = type_poste
+        self.sys_exploitation = sys_exploitation
+        self.adresse_ip = adresse_ip
+        self.statut = statut
+
+        # On laisse id=None car l'identifiant est attribué par autoincrémentation
